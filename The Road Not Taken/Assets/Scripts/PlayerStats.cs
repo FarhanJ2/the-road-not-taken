474485fafe0f2c09b80c861c12a20c08f2ae6055
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2024-05-08 23:24:35 -0400

    Movement Created

---
+++ The Road Not Taken/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private int health = 100;
+    private int maxHealth = 100;
+    private int damage = 10;
+    private int score = 0;
+}
